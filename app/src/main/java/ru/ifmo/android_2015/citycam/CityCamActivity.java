25230727902f36d21c3ca6059efe824464649d8d
Pavlov Ivan <i.pavlov17@gmail.com>
2015-10-25 01:23:26 +0300

    first commit

--- app/src/main/java/ru/ifmo/android_2015/citycam/CityCamActivity.java
+++ app/src/main/java/ru/ifmo/android_2015/citycam/CityCamActivity.java
@@ -1,13 +1,24 @@
 package ru.ifmo.android_2015.citycam;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
+import android.util.JsonToken;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import ru.ifmo.android_2015.citycam.model.City;
+import ru.ifmo.android_2015.citycam.webcams.Webcams;
 
 /**
  * Экран, показывающий веб-камеру одного выбранного города.
@@ -24,6 +35,7 @@ public class CityCamActivity extends AppCompatActivity {
 
     private ImageView camImageView;
     private ProgressBar progressView;
+    private DownloadJsonTask downloadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +55,103 @@ public class CityCamActivity extends AppCompatActivity {
 
         progressView.setVisibility(View.VISIBLE);
 
-        // Здесь должен быть код, инициирующий асинхронную загрузку изображения с веб-камеры
-        // в выбранном городе.
+        if (savedInstanceState != null) {
+            downloadTask = (DownloadJsonTask) getLastNonConfigurationInstance();
+        }
+        if (downloadTask == null) {
+            downloadTask = new DownloadJsonTask(this);
+            downloadTask.execute();
+        } else {
+            downloadTask.attachActivity(this);
+        }
     }
+    enum DownloadState {
+        DOWNLOADING(R.string.downloading),
+        DONE(R.string.done),
+        ERROR(R.string.error);
+
+        // ID строкового ресурса для заголовка окна прогресса
+        final int titleResId;
+
+        DownloadState(int titleResId) {
+            this.titleResId = titleResId;
+        }
+    }
+
+    private class DownloadJsonTask extends AsyncTask<Void, Integer, DownloadState> {
+
+        private Context appContext;
+        private CityCamActivity activity;
+        private DownloadState state = DownloadState.DOWNLOADING;
+
+        DownloadJsonTask(CityCamActivity activity) {
+            this.appContext = activity.getApplicationContext();
+            this.activity = activity;
+        }
+
+        void attachActivity(CityCamActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected DownloadState doInBackground(Void... voids) {
+            try {
+                downloadFile(appContext);
+                state = DownloadState.DONE;
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error downloading file: " + e, e);
+                state = DownloadState.ERROR;
+            }
+            return state;
+        }
+    }
+
+    void downloadFile(Context context) throws IOException {
+        File destFile = FileUtils.createTempExternalFile(context, "json");
+        DownloadUtils.downloadFile(Webcams.createNearbyUrl(this.city.latitude, this.city.longitude).toString(), destFile);
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        JsonReader jsonReader = null;
+        String imageUrl = null;
+        try {
+            inputStreamReader = new InputStreamReader(new FileInputStream(destFile));
+            bufferedReader = new BufferedReader(inputStreamReader);
+            jsonReader = new JsonReader(bufferedReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                final String name = jsonReader.nextName();
+                final boolean isNull = jsonReader.peek() == JsonToken.NULL;
+                if(name.equals("webcams") && !isNull) {
+                    jsonReader.beginObject();
+                    while (jsonReader.hasNext()) {
+                        final String nextName = jsonReader.nextName();
+                        final boolean isNextNull = jsonReader.peek() == JsonToken.NULL;
+                        if(nextName.equals("webcam") && !isNextNull) {
+                            jsonReader.beginArray();
+                            jsonReader.beginObject();
+                            while (jsonReader.hasNext()) {
+                                final String webcamName = jsonReader.nextName();
+                                final boolean webcamNameNull = jsonReader.peek() == JsonToken.NULL;
+                                if(webcamName.equals("preview_url") && !webcamNameNull)
+                                    imageUrl = jsonReader.nextString();
+                                else
+                                    jsonReader.skipValue();
+                            }
+                            jsonReader.endArray();
+                        } else jsonReader.skipValue();
+                    }
+                    jsonReader.endObject();
+                } else jsonReader.skipValue();
+            }
+            jsonReader.endObject();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("IMAGE URLLLLDLL", imageUrl);
+    }
+
 
     private static final String TAG = "CityCam";
 }
