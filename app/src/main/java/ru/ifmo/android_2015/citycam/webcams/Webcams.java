d109ee0dae44c727f0d872b3c63d3b2d36315476
Romanosov <Romansamokhvalov@mail.ru>
2015-12-27 20:25:09 +0300

    Done City Cam

--- app/src/main/java/ru/ifmo/android_2015/citycam/webcams/Webcams.java
+++ app/src/main/java/ru/ifmo/android_2015/citycam/webcams/Webcams.java
@@ -12,7 +12,7 @@ public final class Webcams {
 
     // Зарегистрируйтесь на http://ru.webcams.travel/developers/
     // и вставьте сюда ваш devid
-    private static final String DEV_ID = "Ваш devid";
+    private static final String DEV_ID = "887120c8bb086ea424d775e1c505ab81";
 
     private static final String BASE_URL = "http://api.webcams.travel/rest";
 
